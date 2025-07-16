466ac914b78574f64b52dfd67c5ae6f347b8c889
neeraj <neerajkomatishetti@gmail.com>
2025-07-16 11:30:53 +0530

    counter.js

--- assignments-master/week-2/01-async-js/easy/1-counter.js
+++ assignments-master/week-2/01-async-js/easy/1-counter.js
@@ -3,8 +3,12 @@
 // We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript
 // It should go up as time goes by in intervals of 1 second
 
-let counter = 0;
- for(i = 0; i < 10; i++) {
-    console.log("counter:", counter);
+let counter = 1;
+const intervalId = setInterval(() => {
+    process.stdout.write(`\rcounter: ${counter}`);
+    // console.log("counter:", counter);
     counter++;
- }
+    if(counter > 10) {
+        clearInterval(intervalId);
+    }
+}, 1000);
