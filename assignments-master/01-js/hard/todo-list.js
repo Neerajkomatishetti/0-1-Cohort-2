e1bb8c34b44cc414c83bd6a829c0232344c91850
neeraj <neerajkomatishetti@gmail.com>
2025-07-18 11:28:25 +0530

    answer to todo-list.js

--- assignments-master/01-js/hard/todo-list.js
+++ assignments-master/01-js/hard/todo-list.js
@@ -11,8 +11,54 @@
 */
 
 class Todo {
-  
 
+  constructor() {
+    this.TodoList = [];
+  } 
+  add (todo) {
+    this.TodoList.push(todo);
+  }
+
+  remove (IndexOfTodo) {
+    if (IndexOfTodo >= 0 && IndexOfTodo < this.TodoList.length){
+      this.TodoList.splice(IndexOfTodo, 1);
+    }
+  }
+    
+
+  update (IndexOfTodo, updatedTodo) {
+    if (IndexOfTodo >= 0 && IndexOfTodo < this.TodoList.length){
+      this.TodoList[IndexOfTodo] = updatedTodo;
+    }
+    
+  }
+
+  getAll () {
+    return this.TodoList;
+  }
+
+  get (IndexOfTodo) {
+    if (IndexOfTodo >= 0 && IndexOfTodo < this.TodoList.length){
+      return this.TodoList[IndexOfTodo];
+    }else {
+      return null;
+    }
+  }
+
+  clear () {
+    this.TodoList = [];
+  }
 }
+
+// let Td = new Todo();
+
+// let Atodo = "Make Coffe at 8:00am!";
+
+// Td.add(Atodo);
+// Td.add("Wakeup at 6:00am");
+// Td.add("Make Maggie for afternoon!")
+
+// let todos = Td.getAll()
+// console.log(todos);
 
 module.exports = Todo;
